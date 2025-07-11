4894cf1d8a90219bece8305c7ada5b0cc7ab8b61
serjo <serjo-study@ya.ru>
2025-07-11 12:24:32 +0300

    add react, ts, react router v6, vite, AdmiralDS

---
+++ task-manager/src/main.tsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components';
+import { LIGHT_THEME, FontsVTBGroup, DropdownProvider } from '@admiral-ds/react-ui';
+import App from './App.tsx'
+import './index.css'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
