fd76d7dbaf8ee84801973e4fb61e60ed1b800f2c
serjo <serjo-study@ya.ru>
2025-07-18 18:21:16 +0300

    fix: put unnecessary in comment

--- task-manager/src/app/store/store.ts
+++ task-manager/src/app/store/store.ts
@@ -24,7 +24,7 @@ const saveState = (state: RootState) => {
   }
 };
 
-const preloadedState = loadState();
+// const preloadedState = loadState();
 
 export const store = configureStore({
   reducer: {
